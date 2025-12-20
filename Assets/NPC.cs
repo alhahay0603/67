d78d74170a0234a0860909125f6df30861c0b939
_winrar_x21 <mishaalex35@mail.ru>
2025-12-20 20:18:12 +0800

    Update NPC.cs

--- Assets/NPC.cs
+++ Assets/NPC.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
+    //здоровье NPC
+    int health = 5;
+
+    //уровень NPC
+    int level = 1;
+
+    // cкорость NPC
+    float speed = 1.2f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        health += level;
     }
 
     // Update is called once per frame
